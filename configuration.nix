b0f489aec48db64d9b39830313ceebd90f5e628b
matthewcroughan <matt@croughan.sh>
2025-10-17 05:06:09 +0000

    get rid of unnecessary profiles and code

--- configuration.nix
+++ configuration.nix
@@ -1,34 +1,7 @@
-{ modulesPath, lib, pkgs, config, ... }:
+{ lib, ... }:
 {
-  fileSystems."/".device = "/dev/disk/by-label/nixos";
-  boot.initrd.systemd.emergencyAccess = true;
-  systemd.enableEmergencyMode = true;
-
-  boot.initrd.systemd.enable = true;
-  boot.loader.grub.enable = false;
-  boot.kernelPatches = [
-    {
-      name = "config-enable-zboot";
-      patch = null;
-      structuredExtraConfig = {
-        EFI_ZBOOT = lib.mkForce lib.kernel.yes;
-        KERNEL_ZSTD = lib.mkForce lib.kernel.yes;
-        RD_ZSTD = lib.mkForce lib.kernel.yes;
-      };
-    }
-  ];
-#  boot.loader.systemd-boot = {
-#    enable = true;
-#  };
-  users.users.root.password = "default";
-  #services.userborn.enable = lib.mkForce true;
-  #systemd.sysusers.enable = lib.mkForce false;
-  #services.openssh = {
-  #  enable = true;
-  #  settings = {
-  #    PermitRootLogin = "yes";
-  #    PasswordAuthentication = true;
-  #  };
-  #};
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # Make CI Happy, NixOS wants a rootfs
+  fileSystems."/".device = lib.mkDefault "/dev/disk/by-label/nixos";
+  # NixOS won't build unless grub is disabled
+  boot.loader.grub.enable = lib.mkDefault false;
 }
