04638cd7121721456a78ecd98c71534c19e3033f
matthewcroughan <matt@croughan.sh>
2025-10-18 01:41:39 +0000

    try hardware.graphics.enable

--- configuration.nix
+++ configuration.nix
@@ -4,4 +4,5 @@
   fileSystems."/".device = lib.mkDefault "/dev/disk/by-label/nixos";
   # the toplevel also won't build unless grub is disabled
   boot.loader.grub.enable = lib.mkDefault false;
+  hardware.graphics.enable = true;
 }
