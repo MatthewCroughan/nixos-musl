484c77418a532e0514800667e64589816e092e6b
matthewcroughan <matt@croughan.sh>
2025-10-18 21:16:01 +0000

    set boot.initrd.systemd.enable = true

--- configuration.nix
+++ configuration.nix
@@ -4,4 +4,7 @@
   fileSystems."/".device = lib.mkDefault "/dev/disk/by-label/nixos";
   # the toplevel also won't build unless grub is disabled
   boot.loader.grub.enable = lib.mkDefault false;
+  # scripted initrd is bad, and you can't run the vm output of the
+  # nixosConfigurations without this
+  boot.initrd.systemd.enable = lib.mkDefault true;
 }
