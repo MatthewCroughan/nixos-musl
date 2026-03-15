c4aefb3ee510417a6b285d7f91020724237ca720
matthewcroughan <matt@croughan.sh>
2026-03-15 14:16:42 +0000

    wip removals

--- musl-llvm.nix
+++ musl-llvm.nix
@@ -11,16 +11,16 @@ in
       ## Prevents accidental runtime linkage to llvm bintools
       # gnugrep = super.gnugrep.override { runtimeShellPackage = self.runCommand "neutered" { } "mkdir -p $out"; };
 
-      #coreutils = super.coreutils.overrideAttrs {
-      #  doCheck = false;
-      #};
+      coreutils = super.coreutils.overrideAttrs {
+        doCheck = false;
+      };
 
-      # https://github.com/NixOS/nixpkgs/pull/445833
-      netbsd = super.netbsd.overrideScope (
-        _final: prev: {
-          compat = prev.compat.overrideAttrs (old: { makeFlags = old.makeFlags ++ [ "OBJCOPY=${glibcPkgs.binutils}/bin/strip" ]; });
-        }
-      );
+      ## https://github.com/NixOS/nixpkgs/pull/445833
+      #netbsd = super.netbsd.overrideScope (
+      #  _final: prev: {
+      #    compat = prev.compat.overrideAttrs (old: { makeFlags = old.makeFlags ++ [ "OBJCOPY=${glibcPkgs.binutils}/bin/strip" ]; });
+      #  }
+      #);
       #pam = super.pam.overrideAttrs {
       #  NIX_LDFLAGS = lib.optionalString (super.stdenv.cc.bintools.isLLVM && lib.versionAtLeast super.stdenv.cc.bintools.version "17") "--undefined-version";
       #};
